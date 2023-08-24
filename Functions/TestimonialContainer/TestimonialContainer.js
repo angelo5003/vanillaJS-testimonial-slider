7f67b27562fa5943426427bffabed9666755305a
Tony5300 <tony_tolen@ziggo.nl>
2023-08-24 18:02:02 +0200

    first commit

---
+++ Functions/TestimonialContainer/TestimonialContainer.js
@@ -0,0 +1,59 @@
+const handleTestimonialCreator = (data) => {
+  const reviewData = data;
+
+  // target the div where you want to show/create elements
+  const textContainer = document.querySelector("section");
+
+  reviewData.map((user) => {
+    // create dynamic img container
+    const imageContainer = document.createElement(`div`);
+    // create dynamic review container
+    const reviewInfoContainer = document.createElement(`div`);
+    // create a container for all new content that contains the other divs
+    const reviewOuterContainer = document.createElement(`div`);
+    // create the review paragraph
+    const createReviewParagraph = document.createElement(`p`);
+    createReviewParagraph.textContent = user.review;
+    // create userName paragraph
+    const createUserParagraph = document.createElement(`p`);
+    createUserParagraph.textContent = user.personName;
+    // create user job title
+    const createUserJobTitle = document.createElement(`p`);
+    createUserJobTitle.textContent = user.personTitle;
+    // create the user img container
+    const createUserImgContainer = document.createElement(`img`);
+    // Set the src attribute
+    createUserImgContainer.setAttribute(`src`, user.userPhoto);
+
+    // add the created elements
+    imageContainer
+      .appendChild(createUserImgContainer)
+      .classList.add("image-inner-container");
+
+    reviewInfoContainer
+      .appendChild(createReviewParagraph)
+      .classList.add("review-paragraph");
+
+    reviewInfoContainer
+      .appendChild(createUserParagraph)
+      .classList.add("user-paragraph");
+
+    reviewInfoContainer
+      .appendChild(createUserJobTitle)
+      .classList.add("user-job-title");
+
+    reviewOuterContainer
+      .appendChild(imageContainer)
+      .classList.add("review-image-container");
+    reviewOuterContainer
+      .appendChild(reviewInfoContainer)
+      .classList.add("review-info-container");
+
+    // add all the created elements to the dynamic created div
+    textContainer
+      .appendChild(reviewOuterContainer)
+      .classList.add("review-outer-container");
+  });
+};
+
+export default handleTestimonialCreator;
---
+++ JSONdata.md
@@ -0,0 +1,7 @@
+// {
+// "id": 2,
+// "review": "If you want to lay the best foundation possible I’d recommend taking this course.\nThe depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+// "personName": "John Tarkpor",
+// "personTitle": "Junior Front-end Developer",
+// "userPhoto": "../assets/images/image-john.jpg"
+// }
